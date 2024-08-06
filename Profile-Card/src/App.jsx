c07258122ef163756d27cce2a93efd434205d680
shivPosted <shivpratap621@gmail.com>
2024-08-07 01:09:07 +0530

    first draft of the profile-card challenge done 😃

---
+++ Profile-Card/src/App.jsx
@@ -0,0 +1,40 @@
+import './app.css';
+
+function App() {
+  return (
+    <main className="container">
+      <img src="public/IMG_20240413_195538_976.jpg" alt="" />
+      <About />
+    </main>
+  );
+}
+function About() {
+  return (
+    <div className="info">
+      <h1>Shiv Pratap Singh</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
+        eaque corporis molestias iure ducimus cum, rerum laboriosam unde
+        officia, minima delectus illo, omnis quas ut praesentium culpa accusamus
+        quasi optio.
+      </p>
+      <div className="skill-set">
+        <Skill name="HTML+CSS" emoji="💪" bgColor="blue" />
+        <Skill name="JavaScript" emoji="💪" bgColor="yellow" />
+        <Skill name="Web Design" emoji="💪" bgColor="lightgreen" />
+        <Skill name="Git and GitHub" emoji="👍" bgColor="#E4462C" />
+        <Skill name="React" emoji="💪" bgColor="#53D4F6" />
+        <Skill name="Swelte" emoji="👶" bgColor="#FD3200" />
+      </div>
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.bgColor }}>
+      {props.name}
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+export default App;
