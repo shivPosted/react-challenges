0651898ae86d8ffb5b5db85ff95a1b710c0a522a
shivPosted <shivpratap621@gmail.com>
2024-08-07 13:47:01 +0530

    final form of the challenge with some minor component tweaks 😃

--- Profile-Card/src/App.jsx
+++ Profile-Card/src/App.jsx
@@ -3,10 +3,13 @@ import './app.css';
 function App() {
   return (
     <main className="container">
-      <img src="public/IMG_20240413_195538_976.jpg" alt="" />
+      <Avatar />
       <About />
     </main>
   );
+}
+function Avatar() {
+  return <img src="IMG_20240413_195538_976.jpg" alt="" />;
 }
 function About() {
   return (
@@ -18,17 +21,22 @@ function About() {
         officia, minima delectus illo, omnis quas ut praesentium culpa accusamus
         quasi optio.
       </p>
-      <div className="skill-set">
-        <Skill name="HTML+CSS" emoji="💪" bgColor="blue" />
-        <Skill name="JavaScript" emoji="💪" bgColor="yellow" />
-        <Skill name="Web Design" emoji="💪" bgColor="lightgreen" />
-        <Skill name="Git and GitHub" emoji="👍" bgColor="#E4462C" />
-        <Skill name="React" emoji="💪" bgColor="#53D4F6" />
-        <Skill name="Swelte" emoji="👶" bgColor="#FD3200" />
-      </div>
+      <SkillSet />
     </div>
   );
 }
+function SkillSet() {
+  return (
+    <div className="skill-set">
+      <Skill name="HTML+CSS" emoji="💪" bgColor="blue" />
+      <Skill name="JavaScript" emoji="💪" bgColor="yellow" />
+      <Skill name="Web Design" emoji="👍" bgColor="lightgreen" />
+      <Skill name="Git and GitHub" emoji="👍" bgColor="#E4462C" />
+      <Skill name="React" emoji="👶" bgColor="#53D4F6" />
+    </div>
+  );
+}
+
 function Skill(props) {
   return (
     <div className="skill" style={{ backgroundColor: props.bgColor }}>
