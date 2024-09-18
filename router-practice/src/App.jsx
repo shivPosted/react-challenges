b8995a020d07b07283ad88575c73b3947fe685da
shivPosted <shivpratap621@gmail.com>
2024-09-19 00:32:36 +0530

    linking variours components with react router

--- router-practice/src/App.jsx
+++ router-practice/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
